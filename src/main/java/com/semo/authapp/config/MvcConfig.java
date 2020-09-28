83498be035ed280d0c754d8730cb6553e1ba4651
dian <956461675@qq.com>
2020-09-28 15:00:39 +0800

    security demo

---
+++ src/main/java/com/semo/authapp/config/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.semo.authapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description: mvc 配置
+ * @Author: Liu Guo Dian
+ * @Date: 2020/9/28 14:55
+ * @Version: 1.0
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
